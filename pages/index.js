a262c90f843d301df7a801d23fdfab11cade66be
Manuela1976 <foto@manuela-pagels.de>
2025-04-14 16:58:14 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, textAlign: "center" }}>
+      <h1>Instagram Post Assistent</h1>
+      <p>Willkommen! Die Inhalte erscheinen montags, mittwochs und freitags 😊</p>
+    </div>
+  );
+}
